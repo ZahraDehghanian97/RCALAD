1eb738fe2dc7c4afba55466f9463ca0507555fe7
Zahra Dehghanian <z.dehghanian@aut.ac.ir>
2021-11-01 10:23:19 +0330

    change main

--- toy_experiments/utils/data_gmm.py
+++ toy_experiments/utils/data_gmm.py
@@ -13,6 +13,7 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 from functools import reduce
 
+
 class sample_GMM():
     """ Toy dataset containing points sampled from a gaussian mixture distribution.
 
@@ -22,6 +23,7 @@ class sample_GMM():
     * densities
 
     """
+
     def __init__(self, num_examples, means=None, variances=None, priors=None,
                  **kwargs):
         rng = kwargs.pop('rng', None)
@@ -30,9 +32,9 @@ class sample_GMM():
             rng = np.random.RandomState(seed)
 
         gaussian_mixture = GMM_distribution(means=means,
-                                                       variances=variances,
-                                                       priors=priors,
-                                                       rng=rng)
+                                            variances=variances,
+                                            priors=priors,
+                                            rng=rng)
         self.means = gaussian_mixture.means
         self.variances = gaussian_mixture.variances
         self.priors = gaussian_mixture.priors
@@ -40,9 +42,10 @@ class sample_GMM():
         features, labels = gaussian_mixture.sample(nsamples=num_examples)
         densities = gaussian_mixture.pdf(x=features)
 
-        data ={'samples': features, 'label': labels, 'density': densities}
+        data = {'samples': features, 'label': labels, 'density': densities}
 
         self.data = data
+
 
 class GMM_distribution(object):
     """ Gaussian Mixture Distribution
@@ -61,18 +64,18 @@ class GMM_distribution(object):
     def __init__(self, means=None, variances=None, priors=None, rng=None, seed=None):
 
         if means is None:
-            means = map(lambda x:  10.0 * np.array(x), [[0, 0],
-                                                        [1, 1],
-                                                        [-1, -1],
-                                                        [1, -1],
-                                                        [-1, 1]])
+            means = map(lambda x: 10.0 * np.array(x), [[0, 0],
+                                                       [1, 1],
+                                                       [-1, -1],
+                                                       [1, -1],
+                                                       [-1, 1]])
         # Number of components
         self.ncomponents = len(means)
         self.dim = means[0].shape[0]
         self.means = means
         # If prior is not specified let prior be flat.
         if priors is None:
-            priors = [1.0/self.ncomponents for _ in range(self.ncomponents)]
+            priors = [1.0 / self.ncomponents for _ in range(self.ncomponents)]
         self.priors = priors
         # If variances are not specified let variances be identity
         if variances is None:
@@ -88,7 +91,7 @@ class GMM_distribution(object):
 
     def _sample_prior(self, nsamples):
         return self.rng.choice(a=self.ncomponents,
-                               size=(nsamples, ),
+                               size=(nsamples,),
                                replace=True,
                                p=self.priors)
 
@@ -103,7 +106,7 @@ class GMM_distribution(object):
 
     def _sample_gaussian(self, mean, variance):
         # sampling unit gaussians
-        epsilons = self.rng.normal(size=(self.dim, ))
+        epsilons = self.rng.normal(size=(self.dim,))
 
         return mean + np.linalg.cholesky(variance).dot(epsilons)
 
@@ -116,11 +119,13 @@ class GMM_distribution(object):
                    self.means, self.variances, self.priors)
         return reduce(lambda x, y: x + y, pdfs, 0.0)
 
+
 def plot_GMM(dataset, save_path):
     figure, axes = plt.subplots(nrows=1, ncols=1, figsize=(4.5, 4.5))
     ax = axes
     ax.set_aspect('equal')
-    ax.set_xlim(-3, 3); ax.set_ylim(-3.5, 3.5)
+    ax.set_xlim(-3, 3);
+    ax.set_ylim(-3.5, 3.5)
     # ax.set_xlim([-6, 6])
     # ax.set_ylim([-6, 6])
     # ax.set_xticks([-6, -4, -2, 0, 2, 4, 6])
@@ -131,27 +136,26 @@ def plot_GMM(dataset, save_path):
 
     ax.set_title('GMM')
 
-    x       = dataset.data['samples']
+    x = dataset.data['samples']
     targets = dataset.data['label']
 
-    axes.scatter(x[:, 0], x[:, 1], marker='.', c=cm.Set1(targets.astype(float)/2.0/2.0) , alpha=0.3)
+    axes.scatter(x[:, 0], x[:, 1], marker='.', c=cm.Set1(targets.astype(float) / 2.0 / 2.0), alpha=0.3)
     plt.tight_layout()
     plt.savefig(save_path, transparent=True, bbox_inches='tight')
 
+
 if __name__ == '__main__':
-    means = map(lambda x:  np.array(x), [[0, 0],
-                                     [2, 2],
-                                     [-1, -1],
-                                     [1, -1],
-                                     [-1, 1]])
+    means = list(map(lambda x: np.array(x), [[0, 0],
+                                        [2, 2],
+                                        [-1, -1],
+                                        [1, -1],
+                                        [-1, 1]]))
     std = 0.1
     variances = [np.eye(2) * std for _ in means]
 
-    priors = [1.0/len(means) for _ in means]
+    priors = [1.0 / len(means) for _ in means]
 
-    gaussian_mixture = GaussianMixtureDistribution(means=means,
-                                                   variances=variances,
-                                                   priors=priors)
-    dataset = GaussianMixture(1000, means, variances, priors, sources=('features', ))
+    gaussian_mixture = GMM_distribution(means=means, variances=variances, priors=priors)
+    dataset = sample_GMM(1000, means, variances, priors, sources=('features',))
     save_path = './gmm_data.pdf'
-    draw_GMM(dataset, save_path)
+    plot_GMM(dataset, save_path)
